2060f824bf4fd0d60891b8122d48f9c9a172cc36
Pierre Bermudez <bermudezpierre@gmail.com>
2025-04-02 12:49:39 +0200

    finalise searchbar and profile cards

--- src/compornents/title.jsx
+++ src/compornents/title.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Title() {
   return (
-    <h1 className="text-white text-center font-bold text-9xl m-20">
-      GitHub Finder 🔍
+    <h1 className="text-white text-center font-bold text-8xl m-20">
+      Giitle 🔍
     </h1>
   );
 }
